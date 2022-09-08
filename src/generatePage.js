8bf1b8c9f3f58e8189a03a6d5ee13d57c2e82a4d
JDReeves86 <Reeves.Jacobd@gmail.com>
2022-09-08 10:09:46 -0700

    functionality considered passing

--- src/generatePage.js
+++ src/generatePage.js
@@ -1,7 +1,6 @@
 
-
+//
 const generatePage = (data) => {
-
   return `
 <!DOCTYPE html>
 <html lang="en">
@@ -18,123 +17,118 @@ const generatePage = (data) => {
         <h1 class="title is-1 has-text-centered py-6">Our Team</h1>
     </header>
     <section class="container.is-full-hd p-6" id="card-container">
-      <div class="columns">
-        ${data.map(element => buildCards(element))}
+      <div class="columns is-multiline is-centered">
+        ${buildCards(data)}
       </div>
     </section>
 
 </body>
 <script src="../src/generatePage.js" rel="text/javascript"></script>
 </html>
-
   `
 }
 
+//
 const buildCards = (data) => {
-  switch(data.role) {
-    case 'Manager':
-      return string = 
-      `
-      <div class="column is-one-third">
-        <div class="card">
-          <div class="card-content">
-            <p class="title px-4 py-2">
-              ${data.name}
-            </p>
-            <p class="subtitle">
-              ${data.role}; 
-            </p>
-            <p class="subtitle">
-            ${data.getId()}
-            </p>
-          </div>
-          <footer class="card-footer">
-            <p class="card-footer-item">
-              <span>
-                Email: <a href="${data.getEmail()}">${data.email}</a>
-              </span>
-            </p>
-            <p class="card-footer-item">
-              <span>
-                ${data.getOfficeNumber()}
-              </span>
-            </p>
-          </footer>
-        </div>
-      </div>
-      `;
-    case 'Engineer':
-      return string = 
-      `
-      <div class="column is-one-third">
-        <div class="card">
-          <div class="card-content">
-            <p class="title px-4 py-2">
-              ${data.name}
-            </p>
-            <p class="subtitle">
-              ${data.role}; 
-            </p>
-            <p class="subtitle">
-            ${data.getId()}
-            </p>
-          </div>
-          <footer class="card-footer">
-            <p class="card-footer-item">
-              <span>
-                Email: <a href="${data.getEmail()}">${data.email}</a>
-              </span>
-            </p>
-            <p class="card-footer-item">
-              <span>
-                Find on <a href="${data.getGithub()}" target="none">Github</a>
-              </span>
-            </p>
-          </footer>
-        </div>
-      </div>
-      `;
-    case 'Intern':
-      return string = 
-      `
-    <div class="column is-one-third">
-      <div class="card">
-        <div class="card-content">
-          <p class="title px-4 py-2">
-            ${data.name}
-          </p>
-          <p class="subtitle">
-            ${data.role}; 
-          </p>
-          <p class="subtitle">
-          ${data.getId()}
-          </p>
-        </div>
-        <footer class="card-footer">
-          <p class="card-footer-item">
-            <span>
-              Email: <a href="${data.getEmail()}">${data.email}</a>
-            </span>
-          </p>
-          <p class="card-footer-item">
-            <span>
-              ${data.getSchool()}
-            </span>
-          </p>
-        </footer>
-      </div>
-    </div>
-      `;
-  }
-
+const cardArr = data.map((element) => cardBuilder(element))
+return cardArr.join('')
 }
 
-
-
-
-
-
-
+//
+function cardBuilder(data) {
+  switch(data.role) {
+      case 'Manager':
+        return string = `
+        <div class="column is-4">
+          <div class="card">
+            <div class="card-content">
+              <p class="title px-4 py-2">
+                ${data.name}
+              </p>
+              <p class="subtitle px-4">
+                ${data.role}; 
+              </p>
+              <p class="subtitle px-4">
+              ${data.getId()}
+              </p>
+            </div>
+            <footer class="card-footer">
+              <p class="card-footer-item">
+                <span>
+                  Email: <a href="${data.getEmail()}">${data.email}</a>
+                </span>
+              </p>
+              <p class="card-footer-item">
+                <span>
+                  ${data.getOfficeNumber()}
+                </span>
+              </p>
+            </footer>
+          </div>
+        </div>
+        `;
+      case 'Engineer':
+        return string = `
+        <div class="column is-4">
+          <div class="card">
+            <div class="card-content">
+              <p class="title px-4 py-2">
+                ${data.name}
+              </p>
+              <p class="subtitle px-4">
+                ${data.role}; 
+              </p>
+              <p class="subtitle px-4">
+              ${data.getId()}
+              </p>
+            </div>
+            <footer class="card-footer">
+              <p class="card-footer-item">
+                <span>
+                  Email: <a href="${data.getEmail()}">${data.email}</a>
+                </span>
+              </p>
+              <p class="card-footer-item">
+                <span>
+                  Find on <a href="${data.getGithub()}" target="none">Github</a>
+                </span>
+              </p>
+            </footer>
+          </div>
+        </div>
+        `;
+      case 'Intern':
+        return string = `
+        <div class="column is-4">
+          <div class="card">
+            <div class="card-content">
+              <p class="title px-4 py-2">
+                ${data.name}
+              </p>
+              <p class="subtitle px-4">
+                ${data.role}; 
+              </p>
+              <p class="subtitle px-4">
+              ${data.getId()}
+              </p>
+            </div>
+            <footer class="card-footer">
+              <p class="card-footer-item">
+                <span>
+                  Email: <a href="${data.getEmail()}">${data.email}</a>
+                </span>
+              </p>
+              <p class="card-footer-item">
+                <span>
+                  ${data.getSchool()}
+                </span>
+              </p>
+            </footer>
+          </div>
+        </div>
+        `;
+    }
+}
 
 module.exports = {
     generatePage,
